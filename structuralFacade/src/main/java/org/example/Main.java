f6c89f608d366086cc6b9a4bce14f0ac34ebe23e
Sam1549 <124046389+Sam1549@users.noreply.github.com>
2023-05-16 00:00:33 +0400

    Add files via upload

---
+++ structuralFacade/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        //демонстрация
+        System.out.println(bins.sum("11", "11"));
+        System.out.println(bins.mult("11", "11"));
+    }
+}
